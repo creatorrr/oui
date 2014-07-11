41a2c535c4b29eddc3138d0815426ab6dc00deac
Diwank Singh <diwank.singh@gmail.com>
2014-07-10 17:22:38 -0700

    Moved todos to separate file
    Signed-off-by: Diwank Singh <diwank.singh@gmail.com>

---
+++ TODOS.md
@@ -0,0 +1,14 @@
+# TODO
+
+ - reconstitute NIL as empty list
+ - implement tree fold
+ - add collapse function
+ - fix printer
+ - improve testing function
+ - eval
+ - cond
+ - apply
+ - lamba
+ - add error messages to Parser
+ - add support for dotted lists
+
--- oui.hs
+++ oui.hs
@@ -122,16 +122,3 @@ test = map exec where
   exec (s, p) = (parseProgram s == p, showProgram p == s)
   parseProgram = either nil id . parseOui
   nil = do return [NIL]
-
--- TODO
--- reconstitute NIL as empty list
--- implement tree fold
--- add collapse function
--- fix printer
--- improve testing function
--- eval
--- cond
--- apply
--- lamba
--- add error messages to Parser
--- add support for dotted lists
